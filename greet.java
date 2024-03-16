f1d445ca0f8d5e9a3d96eefc277fc88f7cc798fb
160722747082 <163519747+160722747082@users.noreply.github.com>
2024-03-16 15:35:59 +0530

    Add files via upload

---
+++ greet.java
@@ -0,0 +1,12 @@
+/**
+command line arguments
+*/
+class greet{
+      public static void main(String[] p){
+
+      System.out.println("weclome"+p[0]);
+      int a=Integer.parseInt(p[1]);
+      int b=Integer.parseInt(p[2]);
+      System.out.println(a+b);
+   }
+}
---
+++ mul.java
@@ -0,0 +1,11 @@
+class mul{
+        public static void main(String[] ar)
+     {
+          System.out.println("multipication table");
+          int n=Integer.parseInt(ar[0]);
+          for (int i=1;i<=10;i++)
+  {
+                  System.out.println(n+"*"+i+"="+n*i);
+         }
+   }
+}
