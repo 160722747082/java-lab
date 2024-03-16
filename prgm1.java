bb985f33489fea9bc66f27abf5bcdadb4236eb50
160722747082 <163519747+160722747082@users.noreply.github.com>
2024-03-16 15:57:03 +0530

    Add files via upload

---
+++ prgm1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class FileN{
+public static void main(string[] p)
+   {
+        scanner sc=new scanner(system.in);
+        system.out.printin("enter your name");
+        sting name=sc.next();
+        system.out.printin("weclome"+name);
+    }
+}
