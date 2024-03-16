cf604fde77e95ec27df0a67a904aa86d14c833da
160722747082 <163519747+160722747082@users.noreply.github.com>
2024-03-16 15:58:07 +0530

    Update prgm1.java

--- prgm1.java
+++ prgm1.java
@@ -2,9 +2,9 @@ import java.util.*;
 class FileN{
 public static void main(string[] p)
    {
-        scanner sc=new scanner(system.in);
-        system.out.printin("enter your name");
+        scanner sc=new scanner(System.in);
+        System.out.printin("enter your name");
         sting name=sc.next();
-        system.out.printin("weclome"+name);
+        System.out.printin("weclome"+name);
     }
 }
